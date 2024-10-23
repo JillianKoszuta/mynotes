b0a91bbd3b9602f54fc7d1e2f7a0a53d0101d9a5
JillianKoszuta <jilliankoszuta@gmail.com>
2024-10-23 00:40:55 -0400

    implement router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
-import Header from './components/Header';
-import './App.css';
-import Body from './components/Body';
-import Footer from './components/Footer';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+import './App.css';
+import Header from './components/Header';
+import NotesPage from './pages/NotesPage';
+import SecondPage from './pages/SecondPage';
 
 function App() {
-  return (
-    <div className="App">
-      <Header/>
-      <Body/>
-      <Footer/>
-    </div>
-  );
+	return (
+		<Router>
+			<div className='container dark'>
+				<div className='app'>
+					<Header />
+					<Routes>
+						<Route
+							path='/'
+							element={<NotesPage />}
+						/>
+						<Route
+							path='/n'
+							element={<SecondPage />}
+						/>
+					</Routes>
+				</div>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
---
+++ src/pages/NotesPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import notes from '../assets/data';
+import ListItem from '../components/ListItem';
+const NotesPage = () => {
+	return (
+		<div>
+			{notes.map((note) => (
+				<ListItem note={note} />
+			))}
+		</div>
+	);
+};
+
+export default NotesPage;
