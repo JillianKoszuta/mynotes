9799c3af26ec10f88e64b8109270a96efa48555a
JillianKoszuta <jilliankoszuta@gmail.com>
2024-10-25 23:24:37 -0400

    replace static data with api requests

--- src/pages/NotePage.js
+++ src/pages/NotePage.js
@@ -1,11 +1,23 @@
-import React from 'react';
-import notes from '../assets/data';
+import React, { useState, useEffect } from 'react';
+//import notes from '../assets/data';
 import { Link, useParams } from 'react-router-dom';
 import { ReactComponent as ArrowLeft } from '../assets/arrow-left.svg';
 
 const NotePage = () => {
 	const noteId = useParams().id;
-	let note = notes.find((note) => note.id === Number(noteId)) || {};
+	//let note = notes.find((note) => note.id === Number(noteId)) || {};
+
+	let [note, setNote] = useState(null);
+
+	const getData = async () => {
+		let response = await fetch(`http://127.0.0.1:5000/notes/${noteId}`);
+		let data = await response.json();
+		setNote(data);
+	};
+
+	useEffect(() => {
+		getData();
+	}, []);
 
 	return (
 		<div className='note'>
--- src/pages/NotesPage.js
+++ src/pages/NotesPage.js
@@ -1,8 +1,20 @@
-import React from 'react';
-import notes from '../assets/data';
+import React, { useState, useEffect } from 'react';
+//import notes from '../assets/data';
 import ListItem from '../components/ListItem';
 
 const NotesPage = () => {
+	let [notes, setNotes] = useState([]);
+
+	const getNotes = async () => {
+		let response = await fetch('http://127.0.0.1:5000/notes');
+		let data = await response.json();
+		setNotes(data);
+	};
+
+	useEffect(() => {
+		getNotes();
+	}, []);
+
 	return (
 		<div className='notes'>
 			<div className='notes-header'>
